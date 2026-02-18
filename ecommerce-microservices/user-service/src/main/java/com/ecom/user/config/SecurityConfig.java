cdc1f6e38baeb709150f88854770b93fc7f455af
deepakdey412@gmail.com <deepakdey412@gmail.com>
2026-02-18 22:34:25 +0530

    Add service registory

--- ecommerce-microservices/user-service/src/main/java/com/ecom/user/config/SecurityConfig.java
+++ ecommerce-microservices/user-service/src/main/java/com/ecom/user/config/SecurityConfig.java
@@ -7,17 +7,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
@@ -25,15 +24,24 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
                 .csrf(csrf -> csrf.disable())
-                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/users/register", "api/users/login", "/actuator/**").permitAll().anyRequest().authenticated())
-                .addFilterBefore(jwtAuthenticationFilter , UsernamePasswordAuthenticationFilter.class);
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/users/register",
+                                "/api/users/login",
+                                "/actuator/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
-
-
 }
--- ecommerce-microservices/user-service/src/main/java/com/ecom/user/controller/UserController.java
+++ ecommerce-microservices/user-service/src/main/java/com/ecom/user/controller/UserController.java
@@ -1,9 +1,45 @@
 package com.ecom.user.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ecom.user.dto.LoginRequestDto;
+import com.ecom.user.dto.LoginResponseDto;
+import com.ecom.user.dto.UserCreatedRequestDto;
+import com.ecom.user.dto.UserResponseDto;
+import com.ecom.user.services.IUserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final IUserService userService;
+    public UserController(IUserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDto> register(@Valid @RequestBody UserCreatedRequestDto userCreatedRequestDto) {
+        UserResponseDto response = userService.registerUser(userCreatedRequestDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@Valid @RequestBody LoginRequestDto loginRequestDto) {
+        LoginResponseDto response = userService.loginUser(loginRequestDto);
+        return new ResponseEntity<>(response, HttpStatus.FOUND);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<UserResponseDto> getUser(@PathVariable("id") Long id , Authentication authentication) {
+        UserResponseDto response = userService.getById(id);
+        return new ResponseEntity<>(response, HttpStatus.FOUND);
+    }
+
+
+
+
+
 }
