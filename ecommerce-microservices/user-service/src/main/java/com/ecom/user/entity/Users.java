99fbd068d4efdc92a46e349468cd007fca88cd46
deepakdey412@gmail.com <deepakdey412@gmail.com>
2026-02-18 19:35:16 +0530

    updated the user entity

--- ecommerce-microservices/user-service/src/main/java/com/ecom/user/entity/Users.java
+++ ecommerce-microservices/user-service/src/main/java/com/ecom/user/entity/Users.java
@@ -1,14 +1,42 @@
 package com.ecom.user.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.Instant;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Users {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String password;
+    @Column(nullable = false, unique = true)
+    private String email;
+    private String phone;
+    private String role;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = Instant.now();
+        this.updatedAt = createdAt;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = Instant.now();
+    }
 }
