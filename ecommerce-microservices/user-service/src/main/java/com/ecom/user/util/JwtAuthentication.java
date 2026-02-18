26ee1754da2ac7d63e889bf846ced76e9e13c8e7
deepakdey412@gmail.com <deepakdey412@gmail.com>
2026-02-18 21:01:10 +0530

    Updates

---
+++ ecommerce-microservices/user-service/src/main/java/com/ecom/user/util/JwtAuthentication.java
@@ -0,0 +1,41 @@
+package com.ecom.user.util;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+public class JwtAuthentication extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    public JwtAuthentication(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        final String header = request.getHeader("Authorization");
+        if (header == null || !header.startsWith("Bearer ")) {
+            final String token = header.substring(7);
+            try {
+                jwtUtil.validateToken(token);
+                Long userId = jwtUtil.getUserIdFromToken(token);
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(userId, null, Collections.emptyList());
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }catch (Exception e) {
+                SecurityContextHolder.clearContext();
+            }
+            filterChain.doFilter(request, response);
+        }
+    }
+}
--- ecommerce-microservices/user-service/src/main/java/com/ecom/user/util/JwtUtil.java
+++ ecommerce-microservices/user-service/src/main/java/com/ecom/user/util/JwtUtil.java
@@ -1,8 +1,6 @@
 package com.ecom.user.util;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,9 +9,9 @@ import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
 import java.util.Date;
 
-// spring security methods are changed in 0.12 version
 @Component
 public class JwtUtil {
+
     private final SecretKey key;
     private final long jwtExpirationInMs;
 
@@ -25,23 +23,30 @@ public class JwtUtil {
         this.jwtExpirationInMs = jwtExpirationInMs;
     }
 
-    public String generateToken(Long userId , String email , String role) {
+    public String generateToken(Long userId, String email, String role) {
+
         long now = System.currentTimeMillis();
-         return Jwts.builder()
-                 .subject(String.valueOf(userId))
-                 .claim("email" , email)
-                 .claim("role ", role)
-                 .issusedAt(new Date(now))
-                 .expiration(new Date(jwtExpirationInMs)).singWith(key)
-                 .compact();
+
+        return Jwts.builder()
+                .setSubject(String.valueOf(userId))
+                .claim("email", email)
+                .claim("role", role)
+                .setIssuedAt(new Date(now))
+                .setExpiration(new Date(now + jwtExpirationInMs))
+                .signWith(key)
+                .compact();
     }
 
-    public Jws<Claims> validateTokenk(String token) {
-        return Jwt.parser().verifywithKey().build().parseSignedClaims(token);
+    public Claims validateToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 
     public Long getUserIdFromToken(String token) {
-        Claims claims = validateTokenk(token).getBody();
+        Claims claims = validateToken(token);
         return Long.valueOf(claims.getSubject());
     }
 }
