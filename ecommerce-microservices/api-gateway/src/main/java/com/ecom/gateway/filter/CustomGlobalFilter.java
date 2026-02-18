98bb6b42ee834fe6d27d96a11a8cd9a936a2610a
deepakdey412@gmail.com <deepakdey412@gmail.com>
2026-02-18 22:51:40 +0530

    update

---
+++ ecommerce-microservices/api-gateway/src/main/java/com/ecom/gateway/filter/CustomGlobalFilter.java
@@ -0,0 +1,13 @@
+package com.ecom.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class CustomGlobalFilter implements GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return null;
+    }
+}
