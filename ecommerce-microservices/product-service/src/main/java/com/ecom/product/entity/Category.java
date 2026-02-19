2decdab07675ad5ed5cc061f3fc5853321d2834f
deepakdey412@gmail.com <deepakdey412@gmail.com>
2026-02-19 19:33:27 +0530

    Update

--- ecommerce-microservices/product-service/src/main/java/com/ecom/product/entity/Category.java
+++ ecommerce-microservices/product-service/src/main/java/com/ecom/product/entity/Category.java
@@ -8,18 +8,24 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Entity
+@Table(name = "category_details")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Entity
-@Table(name = "category_details")
 public class Category {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(nullable = false)
     private String categoryName;
+    @Column(nullable = false , length = 200)
     private String categoryDescription;
     private Long parentId;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
     private List<Product> products;
 }
