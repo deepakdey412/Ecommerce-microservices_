0242796c46cfde674ff656979609dbea47f330a1
deepakdey412@gmail.com <deepakdey412@gmail.com>
2026-02-19 19:55:28 +0530

    Updated the dto of product and category

--- ecommerce-microservices/product-service/src/main/java/com/ecom/product/dto/CategoryDto.java
+++ ecommerce-microservices/product-service/src/main/java/com/ecom/product/dto/CategoryDto.java
@@ -1,5 +1,6 @@
 package com.ecom.product.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Data
@@ -9,6 +10,7 @@ import lombok.*;
 public class CategoryDto {
 
     private Long id;
+    @NotBlank(message = "Category name should not empty")
     private String categoryName;
     private String categoryDescription;
     private Long parentId;
--- ecommerce-microservices/product-service/src/main/java/com/ecom/product/dto/ProductDto.java
+++ ecommerce-microservices/product-service/src/main/java/com/ecom/product/dto/ProductDto.java
@@ -1,6 +1,10 @@
 package com.ecom.product.dto;
 
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 @Data
@@ -10,13 +14,23 @@ import lombok.*;
 public class ProductDto {
 
     private Long id;
+    @NotBlank(message = "Product name is required")
     private String name;
+    @NotBlank(message = "Product description is  required")
     private String description;
+    @NotBlank(message = "Product price is  required")
+    @Positive(message = "Price must be positive")
     private Double price;
+    @NotBlank(message = "Product discount price is required")
     private Double discountPrice;
+    @NotBlank(message = "Product quantity is  required")
+    @Min(value = 0 , message = "Quantity must be 0 or more ")
     private Integer quantity;
+    @NotBlank(message = "Product brand name is required")
     private String brand;
+    @NotBlank(message = "Product quantity required")
     private String imageUrl;
+    @NotNull(message = "Category id is required")
     private Long categoryId;
 
 }
