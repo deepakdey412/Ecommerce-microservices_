c24a452606d7fc6da41f34fd4171b3a8a2cce22b
deepakdey412@gmail.com <deepakdey412@gmail.com>
2026-02-19 19:59:26 +0530

    Added Product repository

--- ecommerce-microservices/product-service/src/main/java/com/ecom/product/repository/ProductRepository.java
+++ ecommerce-microservices/product-service/src/main/java/com/ecom/product/repository/ProductRepository.java
@@ -4,6 +4,8 @@ import com.ecom.product.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,4 +14,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByProductCategory_Id(Long categoryId, Pageable pageable);
     Page<Product> findByPriceBetween(Double min, Double max, Pageable pageable);
     Page<Product> findByNameContainingIgnoreCase(String keyword, Pageable pageable);
+
+    @Query("SELECT p FROM Product p " +
+            "WHERE LOWER(p.productName) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "OR LOWER(p.productDescription) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    Page<Product> searchProducts(@Param("keyword") String keyword, Pageable pageable);
+
 }
