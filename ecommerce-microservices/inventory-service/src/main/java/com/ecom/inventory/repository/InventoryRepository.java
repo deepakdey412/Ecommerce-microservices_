224223d3b1a565bc2641f72d441061f311d3b797
deepakdey412@gmail.com <deepakdey412@gmail.com>
2026-02-23 23:37:51 +0530

    added annotationn in inventory repository interface

--- ecommerce-microservices/inventory-service/src/main/java/com/ecom/inventory/repository/InventoryRepository.java
+++ ecommerce-microservices/inventory-service/src/main/java/com/ecom/inventory/repository/InventoryRepository.java
@@ -2,9 +2,11 @@ package com.ecom.inventory.repository;
 
 import com.ecom.inventory.entity.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
     Optional<Inventory> findByskuCode(String skuCode);
 }
